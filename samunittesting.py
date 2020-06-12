784bac58cce21ade92dc0ebda799595fe13d3a62
Sam serrano <sserranoperez@sea.yearup.org>
2020-06-12 09:42:06 -0700

    Samtest

---
+++ samunittesting.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+###########
+#This is where the unit test goes for testing function 
+
+class TestStringMethods(unittest.TestCase):
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+
+
+
+
+	def test_isupper(self):
+
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world'])
+		#Check that s.split falls when the seperator is not a string
+
+		with self.assertRaises(TypeError):
+			s.split(2)
+
+if __name__ == '__main__':
+	unittest.main()
+
+
